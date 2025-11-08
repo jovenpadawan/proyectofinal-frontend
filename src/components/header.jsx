5b660533737b5d0d7d4d39a7f248f797f9c1f5a7
jovenpadawan <pedromiguelreyes.ne@gmail.com>
2025-11-07 20:20:22 -0500

    Inicio de proyecto React, carpeta components y diseño inicial del header

---
+++ src/components/header.jsx
@@ -0,0 +1,10 @@
+import './header.css';
+
+export default function header() {
+    return (
+        <header className='headerContainer'>
+            <img className='imgContainer' src='./sources/images/gmLogo.png'></img>
+            <h1>GameTraking Winnian</h1>
+        </header>
+    );
+}
